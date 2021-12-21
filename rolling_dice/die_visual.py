99a73dd69a1aa63dc232a268fd94386b1b222ef8
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2021-12-21 14:59:47 +0000

    Add Die project

---
+++ rolling_dice/die_visual.py
@@ -0,0 +1,26 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
+from die import Die
+
+die = Die()
+
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+for value in range(1, die.num_sides + 1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+# Visualize the results.
+x_values = list(range(1, die.num_sides + 1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {'title': 'Result'}
+y_axis_config = {'title': 'Frequency of Result'}
+my_layout = Layout(title='Result of rolling dice 1000 times.', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data': data, 'layout': my_layout}, filename='d6.html')
