07be9220093ceda89d6e465a8bdcbe329c4ee011
Dimitar Tsvetanov <dimitarcho93@gmail.com>
2021-12-21 14:59:21 +0000

    Small update

--- random_walk/rw_visual.py
+++ random_walk/rw_visual.py
@@ -7,9 +7,9 @@ rw.fill_walk()
 plt.style.use('classic')
 fig, ax = plt.subplots()
 point_numbers = range(rw.num_points)
-ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=15)
+ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor='none', s=1)
 
-ax.scatter(0,0, c='green', edgecolor='none', s=100)
+ax.scatter(0, 0, c='green', edgecolor='none', s=100)
 ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolor='none', s=100)
 
 ax.get_xaxis().set_visible(False)
